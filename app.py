8d25c7f036308c953a7f076408eec4e9ae5017ca
daniel an <d22an@uwaterloo.ca>
2021-05-31 00:14:10 -0400

    added drop down search

--- app.py
+++ app.py
@@ -20,7 +20,16 @@ def news_bar():
     elif request.method == 'GET':
         temp = request.args.get('char1')
         temp2 = int(request.args.get('num1'))
-        return render_template('base.html', char1=temp, num1=temp2, start=False, context=get_news(False, temp, temp2))
+        source_input = request.args.get('source')
+        print("ASDASDASDASDASDASDASDASD")
+        print(source_input)
+        if source_input == "all":
+            return render_template('base.html', char1=temp, num1=temp2, start=False, context=get_news(False, temp, temp2))
+        else:
+            context = get_news_with_source(False, temp, temp2, source_input)
+            return render_template('base.html', char1=temp, num1=temp2, start=False, context=context)
+
+        
 
 
 def get_news(start, url_query, num_articles):
@@ -39,15 +48,52 @@ def get_news(start, url_query, num_articles):
     desc = []
     news = []
     img = []
-    source = []
+    sources = []
+    url=[]
     for i in range(len(articles)):
         myarticles = articles[i]
         news.append(myarticles['title'])
         desc.append(myarticles['description'])
         img.append(myarticles['urlToImage'])
-        source.append(myarticles['source'])
+        sources.append(myarticles['source'])
+        url.append(myarticles['url'])
 
-    mylist = zip(news, desc, img, source)
+    mylist = zip(news, desc, img, sources, url)
+    return mylist
+
+
+def get_news_with_source(start, url_query, num_articles, source):
+    newsapi = NewsApiClient(api_key=my_api_key)
+    if (start == True):
+        # get headlines
+        topheadlines = newsapi.get_top_headlines(
+            country='ca', category='business')
+        articles = topheadlines['articles']
+    else:
+        data = newsapi.get_everything(
+            q=url_query, language='en', page_size=num_articles)
+        articles = data['articles']
+
+    # get info for the articles
+    desc = []
+    news = []
+    img = []
+    sources = []
+    url=[]
+    for i in range(len(articles)):
+        myarticles = articles[i]
+        print("======================")
+        print(myarticles['source']['name'])
+        print(source)
+        print("======================")
+        if myarticles['source']['name'] == source:
+            news.append(myarticles['title'])
+            desc.append(myarticles['description'])
+            img.append(myarticles['urlToImage'])
+            sources.append(myarticles['source'])
+            url.append(myarticles['url'])
+
+    mylist = zip(news, desc, img, sources, url)
     return mylist
 
 
